0a1b496a37489d66fa7c48d00becb1efc0d27ffd
Andrew Alexander Balogh <andrewalexanderbalogh@gmail.com>
2019-09-09 18:03:18 -0400

    initial maven project setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # openmucProject
 Project that includes the openmuc/jrxtx module
+
+~ set up the debug configuration as shown below
+file:///home/andrew/Desktop/openmucDebugConfig.png
---
+++ src/main/java/com/mitechisys/OpenmucTest.java
@@ -0,0 +1,23 @@
+package com.mitechisys;
+
+import org.openmuc.jrxtx.Parity;
+import org.openmuc.jrxtx.SerialPort;
+import org.openmuc.jrxtx.SerialPortBuilder;
+
+public class OpenmucTest {
+    // see https://openmuc.github.io/jrxtx/javadoc/index.html
+
+    public static void main(String[] args) throws Throwable {
+        System.out.println("Starting Openmuc Test...");
+
+        // some stack-overflow example: https://stackoverflow.com/questions/52697598/java-rxtxcomm-lib-does-not-load-or-is-not-to-be-found
+        try {
+            SerialPort port = SerialPortBuilder.newBuilder("/dev/ttyAMA0").setBaudRate(9600).setParity(Parity.NONE).build();
+        }
+        catch (Throwable throwable){
+            System.out.println(throwable.getMessage());
+        }
+
+        System.out.println("Ending Openmuc Test...");
+    }
+}
